42e9256842309b8efe7f894672be708e0f8bc0dc
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-26 23:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400){
+    result = 'This one is on me!';
+
+
+  }
+  return result
+}
