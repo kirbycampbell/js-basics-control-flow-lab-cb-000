428b036f352c59513ccda88b033813856aaabe93
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-26 23:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue){
   if (someValue <= 400){
     result = 'This one is on me!';
   }
-  else if (someValue <= 2500 || someValue > 2000) {
+  else if (someValue <= 2500 && someValue > 2000) {
     result = 'I will gladly take your thirty bucks.';
   }
   else if (someValue > 2500){
